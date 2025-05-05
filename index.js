ed70572c4e04aa12f82ebcb5c3ca3072b70cce07
Mike <mclancy96@gmail.com>
2025-05-05 15:35:48 -0400

    mondayWork returning correct string

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const saturdayFun = (funActivity = 'roller-skate') => {
     return `This Saturday, I want to ${funActivity}!`
 }
+
+const mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
